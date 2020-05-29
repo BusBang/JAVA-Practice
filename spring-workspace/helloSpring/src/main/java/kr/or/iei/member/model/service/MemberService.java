aa617de1aa736da806c7dc1ef4cd34fa6d862238
BusBang <58544672+BusBang@users.noreply.github.com>
2020-05-29 17:44:39 +0900

    200529(2)

--- spring-workspace/helloSpring/src/main/java/kr/or/iei/member/model/service/MemberService.java
+++ spring-workspace/helloSpring/src/main/java/kr/or/iei/member/model/service/MemberService.java
@@ -1,5 +1,51 @@
 package kr.or.iei.member.model.service;
 
-public class MemberService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import kr.or.iei.member.model.dao.MemberDao;
+import kr.or.iei.member.model.vo.Member;
+
+@Service("memberService")
+public class MemberService {
+	
+	
+	@Autowired
+	@Qualifier("memberDao")
+	private MemberDao dao;
+	public MemberService() {
+		super();
+		System.out.println("memberService 생성 완료");
+	}
+
+	
+	public Member selectOneMember(Member m) {
+		List list = dao.selectOneMember(m);
+		Member member = null;
+		if(!list.isEmpty()) {
+			member = (Member)list.get(0);
+		}
+		return member;
+	}
+
+
+	public int insertMember(Member m) {
+		return dao.insertMember(m);
+	}
+
+
+	public int updateMember(Member m) {
+		return dao.updateMember(m);
+	}
+
+
+	public int deleteMember(Member m) {
+		return dao.deleteMember(m);
+	}
+	
+	
+	
 }
--- spring-workspace/helloSpring/src/main/webapp/index.jsp
+++ spring-workspace/helloSpring/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,10 +9,19 @@
 </head>
 <body>
 	<h1>스프링 프로젝트 생성완료 !!</h1>
+	<c:if test="${empty sessionScope.member }">	
 	<form action = "/login.do" method="post">
 		아이디 : <input type="text" name="memberId"><br>
 		비밀번호 : <input type="password" name="memberPw"><br>
 		<input type="submit" value="로그인">
 	</form>
+	<a href="/joinFrm.do">회원가입</a>
+	</c:if>
+	<c:if test="${not empty sessionScope.member }">
+	<h1>[${sessionScope.member.memberName }]</h1>
+	<hr>
+	<a href="/logout.do">로그아웃</a><br>
+	<a href="/mypage.do">마이페이지</a>
+	</c:if>
 </body>
 </html>
